c4ba95a4855aa5abb583800a28abeff9c345df54
Gabriel Fonseca <gabrielfonsecaoficial@gmail.com>
2021-11-06 16:14:42 -0400

    refactor: refatorando as rotas

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,9 @@
 import express from 'express';
-import { categoriesRoutes } from './routes/categories.routes';
-import { specificationRoutes } from './routes/specification.routes';
+import { router } from './routes';
 
 const app = express();
 app.use(express.json());
 
-//Endpoits
-app.use('/categories', categoriesRoutes);
-app.use('/specifications', specificationRoutes);
+app.use(router);
 
 app.listen(8080, () => console.log("Application started on port 8080! 🚀"));
