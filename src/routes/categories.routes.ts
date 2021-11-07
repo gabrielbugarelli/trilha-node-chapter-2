06c8b66f7c3590cb66881e3f7a34c32f8b034b65
Gabriel Fonseca <gabrielfonsecaoficial@gmail.com>
2021-11-06 22:29:07 -0400

    feat: criando o use case para importar categorias

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,13 +1,25 @@
 import {Request, Response, Router} from 'express';
+import multer from 'multer';
+
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { importCategoryController } from '../modules/cars/useCases/importCategory';
 import { listCategoryController } from '../modules/cars/useCases/listCategories';
 
 export const categoriesRoutes = Router();
 
+const upload = multer({
+  dest: "./tmp"
+})
+
+//endpoints
 categoriesRoutes.post('/', async (request: Request, response: Response) => {
   return createCategoryController.handle(request, response);
 })
 
 categoriesRoutes.get('/', async (request: Request, response: Response) => {
   return listCategoryController.handle(request, response);
+})
+
+categoriesRoutes.post('/import', upload.single("file"), (request: Request, response: Response) => {
+  return importCategoryController.handle(request, response);
 })
