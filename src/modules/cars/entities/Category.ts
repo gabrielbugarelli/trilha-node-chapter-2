bb51d693e8f704e9a941f91ce145d43bb5b3a4a1
Gabriel Fonseca <gabrielfonsecaoficial@gmail.com>
2021-11-11 12:52:51 -0400

    feat: refatorando o model de categoria

--- src/modules/cars/entities/Category.ts
+++ src/modules/cars/entities/Category.ts
@@ -1,9 +1,17 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
 import {v4 as uuidV4} from 'uuid';
 
+@Entity("categories")
 export class Category {
+
+  @PrimaryColumn()
   id?: string;
+
+  @Column()
   name?: string;
   description?: string;
+
+  @CreateDateColumn()
   created_at?: Date;
 
   constructor() {
