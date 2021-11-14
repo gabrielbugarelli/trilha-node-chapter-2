4076e2677899f4b16de9908cd321ef0397c9988f
Gabriel Fonseca <gabrielfonsecaoficial@gmail.com>
2021-11-14 11:13:44 -0400

    feat: conhecendo TSyringe

--- src/modules/cars/useCases/CreateCategory/index.ts
+++ src/modules/cars/useCases/CreateCategory/index.ts
@@ -1,15 +1,15 @@
-import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
-import { CreateCategoryController } from "./CreateCategoryController";
-import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+// import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
+// import { CreateCategoryController } from "./CreateCategoryController";
+// import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
-export default (): CreateCategoryController => {
-  const categoriesRepository = new CategoriesRepository();
-  const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
-  const createCategoryController = new CreateCategoryController(createCategoryUseCase);
+// export default (): CreateCategoryController => {
+//   const categoriesRepository = new CategoriesRepository();
+//   const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
+//   const createCategoryController = new CreateCategoryController(createCategoryUseCase);
 
-  if (!createCategoryController) {
-    throw new Error('Dont create createCategoryController!');
-  }
+//   if (!createCategoryController) {
+//     throw new Error('Dont create createCategoryController!');
+//   }
 
-  return createCategoryController;
-}
+//   return createCategoryController;
+// }
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,7 @@
 import {Request, Response, Router} from 'express';
 import multer from 'multer';
 
-import createCategoryController from '../modules/cars/useCases/createCategory';
+import { CreateCategoryController } from '../modules/cars/useCases/CreateCategory/CreateCategoryController';
 import importCategoryController from '../modules/cars/useCases/importCategory';
 import listCategoryController from '../modules/cars/useCases/listCategories';
 
@@ -11,10 +11,10 @@ const upload = multer({
   dest: "./tmp"
 })
 
+const createCategoryController = new CreateCategoryController();
+
 //endpoints
-categoriesRoutes.post('/', async (request: Request, response: Response) => {
-  return createCategoryController().handle(request, response);
-})
+categoriesRoutes.post('/', createCategoryController.handle);
 
 categoriesRoutes.get('/', async (request: Request, response: Response) => {
   return listCategoryController().handle(request, response);
